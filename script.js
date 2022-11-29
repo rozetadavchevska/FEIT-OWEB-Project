1dac958373fda622b3ebe5f6a0bad4b643c0adb2
rozetadavchevska <rozetadavcevska@gmail.com>
2022-11-29 23:52:18 +0100

    Likes function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var hamburgerMenu = document.querySelector('.hamburger-menu');
 var nav = document.querySelector('.menu');
+
 var header = document.querySelector('.header');
 var sticky = header.offsetTop;
+
+let like = document.getElementsByClassName('fa-heart');
+let count = document.getElementsByClassName('count');
 
 hamburgerMenu.addEventListener('click', ()=>{
     nav.classList.toggle('active');
@@ -19,3 +23,16 @@ function stickyNav (){
         header.classList.remove("sticky");
     }
 }
+
+for (var i = 0 ; i < like.length; i++) {
+    like[i].addEventListener('click' , ()=>{
+        if(like[i].classList.contains("fa-regular")){
+            like[i].classList.remove("fa-regular");
+            like[i].classList.add("fa-solid");
+        }else{
+            like[i].classList.remove("fa-solid");
+            like[i].classList.add("fa-regular");
+        }
+    }) ; 
+ }
+
